4bfccf57c38946b93f7ab9727b6b55cc2f270216
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2024-05-15 15:20:42 +0200

    fix(webapps): add translation handler
    related to camunda/camunda-bpm-platform#4351

--- webapps/frontend/ui/common/scripts/services/locales.js
+++ webapps/frontend/ui/common/scripts/services/locales.js
@@ -20,6 +20,18 @@ var moment = require('camunda-commons-ui/vendor/moment'),
   angular = require('camunda-commons-ui/vendor/angular');
 
 module.exports = function(ngModule, appRoot, appName) {
+  ngModule.factory('sanitizeMissingTranslationKey', [
+    '$translateSanitization',
+    function($sanitize) {
+      return function(translationKey) {
+        // avoid escape already escaped html character entities
+        if (!['&lt;', '&gt;', '&lt;=', '&gt;='].includes(translationKey)) {
+          return $sanitize.sanitize(translationKey, 'text', 'escape');
+        }
+      };
+    }
+  ]);
+
   ngModule.factory('localeLoader', [
     '$q',
     '$http',
@@ -101,6 +113,9 @@ module.exports = function(ngModule, appRoot, appName) {
     '$translateProvider',
     'configurationProvider',
     function($translateProvider, configurationProvider) {
+      $translateProvider.useMissingTranslationHandler(
+        'sanitizeMissingTranslationKey'
+      );
       var avail = configurationProvider.getAvailableLocales();
       var fallback = configurationProvider.getFallbackLocale();
 
